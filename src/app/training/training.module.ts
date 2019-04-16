dcd108f02e196e7e06765825f7184959ee99ccf1
bhenriq-souza <bhenriq.souza@gmail.com>
2019-04-16 16:12:55 -0300

    Adding the Current Training component

--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CurrentTrainingComponent } from './current-training/current-training.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CurrentTrainingComponent],
   imports: [
     CommonModule
   ]
