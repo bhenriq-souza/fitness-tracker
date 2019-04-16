aab75f188a28f7e922a3622545f7de1a695091be
bhenriq-souza <bhenriq.souza@gmail.com>
2019-04-16 16:14:36 -0300

    Adding the Past Trainings component

--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CurrentTrainingComponent } from './current-training/current-training.component';
 import { NewTrainingComponent } from './new-training/new-training.component';
+import { PastTrainingsComponent } from './past-trainings/past-trainings.component';
 
 @NgModule({
-  declarations: [CurrentTrainingComponent, NewTrainingComponent],
+  declarations: [CurrentTrainingComponent, NewTrainingComponent, PastTrainingsComponent],
   imports: [
     CommonModule
   ]
