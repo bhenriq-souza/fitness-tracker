3342d0893cc54718ee41c594d1e921e3b8a6313c
bhenriq-souza <bhenriq.souza@gmail.com>
2019-04-28 15:11:55 -0300

    Adding toolbar features

--- src/app/app-material/app-material.module.ts
+++ src/app/app-material/app-material.module.ts
@@ -8,7 +8,8 @@ import {
     MatInputModule,
     MatDatepickerModule,
     MatCheckboxModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule
   } from '@angular/material';
 
 @NgModule({
@@ -21,7 +22,8 @@ import {
     MatDatepickerModule,
     MatMomentDateModule,
     MatCheckboxModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule
   ],
   exports: [
     MatButtonModule,
@@ -30,7 +32,8 @@ import {
     MatDatepickerModule,
     MatMomentDateModule,
     MatCheckboxModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 export class AppMaterialModule { }
